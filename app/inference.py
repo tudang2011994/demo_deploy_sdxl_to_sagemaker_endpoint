87a803614e5b4b3eb7a0ac09c0bd944e8be3799e
tudang2011994 <tudang2011994@gmail.com>
2026-03-29 03:57:06 -0700

    initial project

---
+++ app/inference.py
@@ -0,0 +1,40 @@
+import torch
+
+from diffusers  import StableDiffusionXLInpaintPipeline
+
+from PIL import Image
+import base64, io
+
+#Init Pipe
+def model_fn(model_dir):
+    pipe = StableDiffusionXLInpaintPipeline.from_pretrained(
+        "", torch_dtype = torch.float16, variant ='fp16', use_safetensors= True
+    ).to('cuda')
+
+    return pipe
+
+
+def predict_fn(input_data,input_mask, pipe):
+    #Get request data
+    prompt = input_data.get('prompt')
+    img_b64 = input_data.get('image')
+    mask_b64 = input_data.get('mask')
+
+    #Convert image from json text to PIL Image
+    init_img = Image.open(io.BytesIO(base64.b64decode(img_b64))).convert('RGB')
+    mask_img = Image.open(io.BytesIO(base64.b64decode(mask_b64))).convert('RGB')
+
+    #Inference model inpainting
+    output = pipe(
+        prompt= prompt,
+        image = init_img,
+        mask = mask_img,
+        strength= 0.99,
+        num_inference_step = 30
+    ).images[0]
+
+    #Convert image and sent response back
+    buffer = io.BytesIO()
+    output.save(buffer, format='PNG')
+    
+    return base64.b64encode(buffer.getvalue()).decode('utf-8') 
